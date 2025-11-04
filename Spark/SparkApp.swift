e1cfa0029611ccffc982a7a57059c7521dc32514
YungJulius <juliusjung1306@gmail.com>
2025-11-03 22:32:17 -0500

    Initial Commit

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Julius  Jung on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
